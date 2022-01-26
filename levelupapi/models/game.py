a6b1796aee8d1c1c1e26adda2aaf76f79389006a
Sullivan Pierce <sullypierce@gmail.com>
2022-01-26 13:22:34 -0500

    added views for gametypes and games

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 from levelupapi.models.gamer import Gamer
+from .gametype import Gametype
 
 class Game(models.Model):
 
     name = models.CharField(max_length=15)
     description = models.CharField(max_length=50)
     gamer = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    number_of_players = models.IntegerField()
+    maker = models.CharField(max_length=50)
+    skill_level = models.CharField(max_length=50)
+    gametype = models.ForeignKey(Gametype, on_delete=models.CASCADE)
