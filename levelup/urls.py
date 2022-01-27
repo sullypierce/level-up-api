537fa8f200de5b0610d18a602baa4acd4352452c
Sullivan Pierce <sullypierce@gmail.com>
2022-01-26 19:08:44 -0500

    created events view and url, tested with postman

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import GameTypes, Games
+from levelupapi.views import Games, GameTypes, Events
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', Games, 'game')
+router.register(r'events', Events, 'event')
 
 urlpatterns = [
     path('', include(router.urls)),
