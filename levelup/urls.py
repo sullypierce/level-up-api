040cf563f6e8914442645e03297ea9a0af830a35
Sullivan Pierce <sullypierce@gmail.com>
2022-01-27 17:10:38 -0500

    fixed small variable name errors, added profile view to return user info and their registered events

--- levelup/urls.py
+++ levelup/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import include
 from django.urls import path
 from levelupapi.views import register_user, login_user
 from rest_framework import routers
-from levelupapi.views import Games, GameTypes, Events
+from levelupapi.views import Games, GameTypes, Events, Profile
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'gametypes', GameTypes, 'gametype')
 router.register(r'games', Games, 'game')
 router.register(r'events', Events, 'event')
+router.register(r'profile', Profile, 'profile')
 
 urlpatterns = [
     path('', include(router.urls)),
